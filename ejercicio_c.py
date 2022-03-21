836143b9bee7987512855cfa3074d116db2f3e1d
maariagarrcia <mrrmn@myuax.com>
2022-03-21 17:41:02 +0100

    inicio de programa

--- ejercicio_c.py
+++ ejercicio_c.py
@@ -1,6 +1,6 @@
 # Ejercicio diamante de la herencia múltiple
 
-## El problema del diamante surge cuando dos subclases B y C heredan de la superclase superior A, y la subclase
+# El problema del diamante surge cuando dos subclases B y C heredan de la superclase superior A, y la subclase
 # inferior D hereda de B y C (Con un diagrama podemos observarlo mejor y vemos porque se conoce como "el problema del diamante"). 
 # Entonces el problema empieza cuando un método de D llama a un método en A ya que no se
 # sabe si hereda de B o de C. 
@@ -8,6 +8,8 @@
 # En los diferentes lenguajes de programación hay una resolución diferente. Python da como solución
 # una lista de clases que se buscan de izquierda a derecha y de abajo a arriba  y luego elimina todas 
 # las apariciones de una clase repetida menos la última. 
+#
+# Ejercicio resuelto con el método super 
 
 
 ### C L A S E S
@@ -18,20 +20,17 @@ class A:
     def __init__(self, a):
         self.a = a
 
-
-# Subclase que hereda de clase base
+# Subclase que hereda de clase A
 class B(A):
     # Uso de **kwargs en una función se usa para pasar, de forma opcional, un número variable de argumentos 
     # con nombre. El parámetro recibe los argumentos como un diccionario.
     # Metodo Constructor
     def __init__(self, b, **kwargs):
-        # Super nos permite invocar y conservar un método o atributo de una clase base 
-        # desde una subclase sin necesidad de nombrarla explícitamente.
+        # Super nos permite acceder desde la subclase  a metodos  o atributos de la clase base.
         super().__init__(**kwargs)
         self.b = b
 
-
-# Subclase que hereda de clase base
+# Subclase que hereda de clase A
 class C(A):
     # Metodo constructor 
     def __init__(self, c, **kwargs):
@@ -44,8 +43,22 @@ class D(B, C):
     def __init__(self, a1, b1, c1):
         super().__init__(a = a1, b= b1, c = c1)
     
+
 ###
 # I N I C I O   P R O G R A M A
-#
+###
+
+# Instanciar
+d = D(1, 2, 3)
+print(isinstance(d, A), isinstance(d, B), isinstance(d, C)) ## COMPRUEBA si d es una instancia de A,B,C
+print(d.a, d.b, d.c) ### Imprimirá 1 ,2 ,3
+
+## Todos los objetoos en Python heredan, en primera instancia, de OBJECT. Y se puede comprobar con el mro() .
+#  Orden de Resolución de Métodos. Devuelve una lista de tipos de los que deriva la clase, en el orden en que se buscan los métodos.
+print(A.mro())
+print(B.mro())
+print(C.mro())
+print(D.mro())
+
 
 
